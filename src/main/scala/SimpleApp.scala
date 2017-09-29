122b9d24cdb00e7d02b00b07700a6d27d19ad0c4
dungltr <dung80@gmail.com>
2017-09-29 11:01:25 +0200

    SQL

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,13 +2,22 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   def main(args: Array[String]) {
     println("\n Hello world")
-    testSQL()
-    val logFile = "/usr/local/Cellar/apache-spark/2.1.0/README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+    val df = spark.read.json("/Users/letrung/Documents/people.json")
+
+    // Displays the content of the DataFrame to stdout
+    df.show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
     println("\n Goodbye")
     spark.stop()
   }
