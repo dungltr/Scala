3c8a63f5b4fe06a21c5a83a319eeda34cebc1d84
dungltr <dung80@gmail.com>
2017-09-29 10:57:26 +0200

    QL

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   def main(args: Array[String]) {
     println("\n Hello world")
+    testSQL()
     val logFile = "/usr/local/Cellar/apache-spark/2.1.0/README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
@@ -10,5 +11,23 @@ object SimpleApp {
     println(s"Lines with a: $numAs, Lines with b: $numBs")
     println("\n Goodbye")
     spark.stop()
+  }
+  def testSQL(): Unit ={
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+    val df = spark.read.json("examples/src/main/resources/people.json")
+
+    // Displays the content of the DataFrame to stdout
+    df.show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
   }
 }
